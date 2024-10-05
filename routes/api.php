c499d4113f603df14308e3dd28a5d0c2f83e9c66
Joseph Holonou <yao.holonou@hotmail.de>
2024-10-05 21:49:28 +0200

    add route for consumer

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
@@ -10,4 +11,11 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('categories', CategoryController::class);
+    Route::post('/categories/comsummer', function(Request $query) {
+        Log::info($query);
+        return response()->json([
+            'message' => 'consumer receive',
+            'data' => $query->all()
+        ]);
+    });
 });
